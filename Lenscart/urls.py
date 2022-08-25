516ff29fa4d8d700bd0ac3e4653b98c32adf99cc
Yasoob Raza <yasoob.raza7@gmail.com>
2022-08-26 01:37:09 +0500

    adding host

--- Lenscart/urls.py
+++ Lenscart/urls.py
@@ -18,13 +18,13 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
-# from django.views.static import serve
-# from django.urls import path
+from django.views.static import serve
+from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    # path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    # path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
+    path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
