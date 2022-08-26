77e5a5dbefc101ed0eb9edb041c76fed460637d1
Yasoob Raza <yasoob.raza7@gmail.com>
2022-08-26 10:45:12 +0500

    new commit

--- Lenscart/settings.py
+++ Lenscart/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'Lenscart/static/')
 STATIC_URL = 'static/'
 # managing media
 MEDIA_ROOT= os.path.join(BASE_DIR, "media")
